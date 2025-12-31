837cf2316a6ae4dba666f65a68c3304746f353e7
Taffazzul-Bin-Azam <140482510+Taffazzul-Bin-Azam@users.noreply.github.com>
2025-12-31 20:49:35 +0530

    fixed routes issue

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -13,7 +13,7 @@ const razorpay = new Razorpay({
 //config variables
 const currency = "inr";
 const deliveryCharge = 50;
-const frontend_URL = "http://localhost:5173";
+const frontend_URL = "https://food-delivery-app-frontend-zf3n.onrender.com/";
 
 // Placing User Order for Frontend using stripe
 const placeOrder = async (req, res) => {
